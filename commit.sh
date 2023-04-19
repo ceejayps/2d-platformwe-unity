4f9e7c2810db06eb65203d5c430c4eba39b92782
ceejayps <ceejayps1308@gmail.com>
2023-04-19 07:21:38 -0500

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 
-
-for file in $(git status | grep 'modified:' | awk '{print $2}'); do
+for file in $(git status | grep 'modified:' | awk '{print $2}' | grep 'My project/Assets/tile map/'); do
     echo "Committing changes to $file..."
     git add $file
     git commit -m "Update $file"
 done
-
